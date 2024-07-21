287110899faee14f1c0d50023e3199d67e30c5fd
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2024-07-21 11:09:56 +0100

    muti tread and refactor

--- demo.py
+++ demo.py
@@ -14,6 +14,8 @@ from mediapipe.tasks.python import BaseOptions, vision
 import mediapipe as mp
 import os
 
+import concurrent.futures
+
 # Facial recognition
 from deepface import DeepFace
 
@@ -57,7 +59,7 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 options = vision.GestureRecognizerOptions(base_options=base_options, running_mode=VisionRunningMode.LIVE_STREAM,
                                           result_callback=setResults)
 sign_recognizer = vision.GestureRecognizer.create_from_options(options)
-frame_number = 0
+
 
 # end pose detection
 
@@ -80,49 +82,32 @@ cam.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 fps = cam.get(cv.CAP_PROP_FPS)
 print(f"Camera FPS: {fps}")
 
-while True:
-    result, image = cam.read()
+def get_emotion_results_from_models(image, frame_number):
+    emotions = {
+        "pose": None,
+        "attention": None,
+        "face": None
+    }
 
-    # tracking frame number for pose detection
-    frame_number += 1
-
-    # output image
-    out_img = Image.new('RGB', (screen_width, screen_height), color = (153, 153, 255))
-    out_img_draw = ImageDraw.Draw(out_img)
-    # image successfully read from webcam
-    if result:
-
-        # attention
-        attention_out = attn.get_x_y_from_image(image)
-        # attention can fail if no face detected
-        if attention_out[0] is not False:
-
-            # Draw circle where gaze detected
-            out_img_draw.arc([(attention_out[0][0]-25, attention_out[0][1]-25),(attention_out[0][0]+25, attention_out[0][1]+25)], start=0, end=360, fill=(255, 255, 255))
-            out_img_draw.text((1000, 10), f"Gaze location : {int(attention_out[0][0])}, {int(attention_out[0][1])}", font=font)
-            print(attention_out[0][0])
-
-        # body pose
-        frame_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    def process_pose(img, frm_num):
+        body_pose_image = img.copy()
+        frame_rgb = cv.cvtColor(body_pose_image, cv.COLOR_BGR2RGB)
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-        sign_recognizer.recognize_async(mp_image, frame_number)
+        sign_recognizer.recognize_async(mp_image, frm_num)
         pose_results = pose.process(frame_rgb)
-        mp_drawing.draw_landmarks(image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        return pose_results
 
-        # facial expression
-        # Convert the image to grayscale
-        gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    def process_attention(img):
+        attention_out = attn.get_x_y_from_image(img)
+        return attention_out
 
-        # Detect faces in the image
+    def process_face(img):
+        gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
         faces = face_cascade.detectMultiScale(gray)
-
-        # Check if any faces are detected
         if len(faces) > 0:
             try:
-                # Analyze the image for emotions
-                analysis_result = DeepFace.analyze(image, actions=['emotion'], enforce_detection=False)
+                analysis_result = DeepFace.analyze(img, actions=['emotion'])#, enforce_detection=False)
                 dominant_emotion = analysis_result[0]['dominant_emotion']
-                print(f"Dominant Emotion: {dominant_emotion}")
             except ValueError as ve:
                 print(f"ValueError: {ve}")
                 dominant_emotion = "No emotion detected"
@@ -132,16 +117,79 @@ while True:
         else:
             print("No faces detected in the image.")
             dominant_emotion = "No emotion detected"
+        return dominant_emotion
 
-        # Display the dominant emotion on the image
-        if dominant_emotion != "No emotion detected":
-            out_img_draw.text((10, 10), dominant_emotion, font=font)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        pose_future = executor.submit(process_pose, image, frame_number)
+        attention_future = executor.submit(process_attention, image)
+        face_future = executor.submit(process_face, image)
+
+        emotions["pose"] = pose_future.result()
+        emotions["attention"] = attention_future.result()
+        emotions["face"] = face_future.result()
+
+    return emotions
+
+
+def draw_pose_result_on_image(img, pose_landmarks, out_img):
+    img = img.copy()
+    mp_drawing.draw_landmarks(img, pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    # Get the image as a PIL image and resize it to fill the screen
+    # Resize the image using OpenCV
+    resized_image = cv.resize(img, (screen_width, screen_height), interpolation=cv.INTER_AREA)
+
+    # Convert the resized image to a PIL image
+    resized_image_pil = Image.fromarray(resized_image)
+
+    # Paste the resized image onto the output image
+    out_img.paste(resized_image_pil, (0, 0))
+
+
+def draw_attention_result(attention_out, out_img_draw):
+    #attention can fail if no face detected
+    if attention_out[0] is not False:
+        # Draw circle where gaze detected
+        out_img_draw.arc([(attention_out[0][0] - 25, attention_out[0][1] - 25),
+                          (attention_out[0][0] + 25, attention_out[0][1] + 25)], start=0, end=360,
+                         fill=(255, 255, 255))
+        out_img_draw.text((1000, 10), f"Gaze location : {int(attention_out[0][0])}, {int(attention_out[0][1])}",
+                          font=font)
+        print(attention_out[0][0])
+
+
+def draw_face_result(dominant_emotion, out_img_draw):
+    out_img_draw.text((10, 10), dominant_emotion, font=font)
+
+
+def detect_image_emotion(img, frame_num):
+
+    out_img = Image.new('RGB', (screen_width, screen_height), color = (153, 153, 255))
+    out_img_draw = ImageDraw.Draw(out_img)
+
+    results = get_emotion_results_from_models(img, frame_num)
+
+    draw_pose_result_on_image(img, results["pose"].pose_landmarks, out_img)
+    draw_attention_result(results["attention"], out_img_draw)
+    draw_face_result(results["face"], out_img_draw)
+
+    cv.imshow("Demo", np.asarray(out_img))  # This will open an independent window
+
+
+
+frame_number = 0
+
+while True:
+    result, image = cam.read()
+
+    # tracking frame number for pose detection
+    frame_number += 1
+
+    # image successfully read from webcam
+    if result:
+        detect_image_emotion(image, frame_number)
     else:
         print("Failed to get image from webcam")
-
-    out_img.paste(Image.fromarray(image), box=(0,0))
-
-    cv.imshow("Demo", np.asarray(out_img)) # This will open an independent window
 
     if cv.waitKey(1) & 0xFF==ord('q'): # quit when 'q' is pressed
         cam.release()
