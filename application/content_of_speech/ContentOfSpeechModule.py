776157e9538fbb194bb469522141cd6e0e6154dc
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2024-07-22 17:51:19 +0100

    Update ContentOfSpeechModule.py

--- application/content_of_speech/ContentOfSpeechModule.py
+++ application/content_of_speech/ContentOfSpeechModule.py
@@ -131,15 +131,15 @@ class ContentOfSpeechEmotionRecognizer:
         self.speech_to_text = GoogleSpeechToText()
         self.emotion_results = []
 
-    async def get_emotion(self, text):
+    def get_emotion(self, text):
         result = self.pipeline(text)
         emotions = [label['label'] for label in result[0] if label['score'] > 0.3]
         return text, emotions
 
     async def run(self):
 
-        async def emotion_handler(time_seconds, transcript):
-            text, emotions = await self.get_emotion(transcript)
+        def emotion_handler(time_seconds, transcript):
+            text, emotions = self.get_emotion(transcript)
             emotion_str = ", ".join(emotions)
             self.emotion_results.append((time_seconds, text, emotion_str))  # Store in global list
 
@@ -151,7 +151,7 @@ class ContentOfSpeechEmotionRecognizer:
                 for content in audio_generator
             )
             responses = self.speech_to_text.client.streaming_recognize(self.speech_to_text.streaming_config, requests)
-            await stream.listen_print_loop(responses, emotion_handler)
+            stream.listen_print_loop(responses, emotion_handler)
 
 
 
@@ -228,7 +228,7 @@ class MicrophoneStream:
                     break
             yield b"".join(data)
 
-    async def listen_print_loop(self, responses, result_handler):
+    def listen_print_loop(self, responses, result_handler):
         num_chars_printed = 0
         for response in responses:
             if not response.results:
@@ -246,7 +246,7 @@ class MicrophoneStream:
                 time_seconds = result.result_end_time.seconds + result.result_end_time.microseconds / 1e6
 
                 # Call the provided result_handler function
-                await result_handler(time_seconds, transcript)
+                result_handler(time_seconds, transcript)
 
                 if re.search(r"\b(exit|quit)\b", transcript, re.I):
                     print("Exiting..")
